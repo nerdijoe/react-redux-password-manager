0e7b8b93005e0ae6abe1743ef01c936efc60f369
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-26 11:26:29 +0700

    added component nav, home, passwordform

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import './App.css';
+import Home from './components/Home'
+import Nav from './components/Nav'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div>
+          <Nav />
+
+          <Route path="/" component={Home} />
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
